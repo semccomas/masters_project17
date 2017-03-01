f330b7f7717cf98082429c2006d7c9888613328e
semccomas <semccomas@gmail.com>
2017-03-01 10:51:29 +0100

    update to profile markers script

--- profile_markers.py
+++ profile_markers.py
@@ -1,6 +1,8 @@
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt 
+import matplotlib.patches as mpatches
+
 
 data = pd.read_csv('~/Desktop/Masters_Thesis/data/data_original_with_all_protein_id.csv', index_col = 1)
 
@@ -12,11 +14,27 @@ conc = data.loc[:, [col for col in data if 'pmol/mg' in col]].drop('Average conc
 conc = conc.drop('Concentration (pmol/mg) J3', 1)
 conc = conc.drop(conc.columns[-3:], 1)
 
+col = list(conc)
+dct = {}
+for val in col:
+	dct[val] = val[-3:]
+conc = conc.rename(columns = dct)
+
 markers_df = conc.loc[markers].transpose()
 markers_df = markers_df.apply(np.log2)
-colors = ['red','red','red','blue','blue','yellow','yellow','yellow','green','green','green','green','black']
+colors = ['red','red','red','blue','blue','orange','orange','orange','green','green','green','green','black']
+
+#fig= plt.figure()
+#ax = fig.add_subplot(1,1,1)
 
 plt.rcParams.update({'font.size': 6})
 markers_df.plot(legend = False, color = colors)
 
+EP = mpatches.Patch(color='red', label='Epithelium')
+ED = mpatches.Patch(color='blue', label='Endothelium')
+SM = mpatches.Patch(color='orange', label='Smooth Muscle')
+BM = mpatches.Patch(color='green', label='Basement Membrane')
+PARK = mpatches.Patch(color='black', label='PARK7/ DJ1')
+
+plt.legend(handles=[EP, ED, SM, BM, PARK], loc=2)
 plt.show()
