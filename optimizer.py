af900cdd67766a7a007da3645fd93ff9f6885912
semccomas <semccomas@gmail.com>
2017-03-15 16:18:11 +0100

    update 15 march

--- optimizer.py
+++ optimizer.py
@@ -82,40 +82,47 @@ TC = (TCL_for_c1 * TP_for_c1) + (TCL_for_c2 * TP_for_c2)
 
 
 
-C = pd.read_csv('~/Desktop/Masters_Thesis/data/liver_data/optimizer_small_data_liver.csv', index_col = 0)
-C = np.asarray(C)
+C_df = pd.read_csv('~/Desktop/Masters_Thesis/data/liver_data/optimizer_small_data_liver.csv', index_col = 0)
+C = np.asarray(C_df)
 C[C == 0] = 0.00000000000001
-np.random.seed(1)
-CL = np.random.rand(2, np.shape(C)[1])  #make the CL guess array, it's as wide as as many samples as we have, and 2 tall, one for each cell type (pretending we have two for now)
-P = np.random.rand(np.shape(C)[0], 2)   #make the P guess array, it's 2 wide, one for each cell type and it's as long as we have proteins
+#np.random.seed(1)
+#to = np.random.uniform(low = 0.0, high = 0.8, size = np.shape(tot))
+CL = np.random.uniform(low = 0.0001, high = 0.001, size = (2, np.shape(C)[1]))  #make the CL guess array, it's as wide as as many samples as we have, and 2 tall, one for each cell type (pretending we have two for now)
+P = np.random.uniform(low = 0.0001, high = 0.001, size = (np.shape(C)[0], 2))   #make the P guess array, it's 2 wide, one for each cell type and it's as long as we have proteins
 
 #get shapes to make variable change easier
 CL_shape = np.shape(CL)
 P_shape = np.shape(P)
-
+cell_num = 2 #(change this if you have more cell lines)
 ##########################
 ##optimizer part
 #######################
 
-warnings.filterwarnings('ignore')  #this can be dangerous so it might not be good to have here but res likes to get mad when the array is variable values
+#warnings.filterwarnings('ignore')  #this can be dangerous so it might not be good to have here but res likes to get mad when the array is variable values
 
 def objective(tot):
 	#get back original arrays, res only works off one guess
 	CL_reshaped, P_reshaped = np.split(tot, [len(CL)])
-
 	#first reshape CL and P. I know it's a lot of variables but lenCL makes it that way
 	CL_reshaped = np.reshape(CL_reshaped, CL_shape)
 	P_reshaped = np.reshape(P_reshaped, P_shape)
-
 	#get an array for each cell line so we can make Cguess
 	CL1 = CL_reshaped[0][np.newaxis] #same shape as TCL_for_c1 (just more samples). Is wider than is tall which is what we want
 	CL2 = CL_reshaped[1][np.newaxis]
 	P1 = P_reshaped[:,0][np.newaxis].transpose()
 	P2 = P_reshaped[:,1][np.newaxis].transpose()
-
 	Cguess = (CL1 * P1) + (CL2 * P2)
-	return np.mean(abs(np.log10(Cguess/C)))   #we have the mean in here because res does not like to have an array as the output
-	#return Cguess
+	return (abs(np.log10(Cguess/C)))   #we have the mean in here because res does not like to have an array as the output
+	#return Cguess #np.mean(np.log(Cguess/C))
+
+
+def sample_comp_constraint(tot):
+	for x in xrange(len(CL)/cell_num):
+		return np.sum(np.reshape(tot[:len(CL)],CL_shape)[:,x]) - 1.0  #the sum of the two slices (which == cell line 1 and cell line 2 in the sample)
+
+def no_negative(tot):
+	for val in tot:
+		return val 
 
 
 #this is for the minimizer input 
@@ -123,7 +130,9 @@ CL = CL.flatten()
 P = P.flatten()   
 tot = np.concatenate([CL, P])
 
-res = minimize(objective, tot, method = 'nelder-mead')
+con1 = {'type': 'eq', 'fun': sample_comp_constraint, 'jac': None}
+cons = [con1]
+res = minimize(objective, tot, method = 'SLSQP', constraints = cons)#, jac = False)
 print res        #right now there is no change between x and res x 
 
 
