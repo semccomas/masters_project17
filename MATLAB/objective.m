18d0ddb70bf094c3d53749ad40027e71c6b9085b
semccomas <semccomas@gmail.com>
2017-04-19 10:01:49 +0200

    update on matlab scripts to push to windows comp

--- MATLAB/objective.m
+++ MATLAB/objective.m
@@ -1,29 +1,28 @@
 %input is P, CL. Out is (P*CL/C). C is referenced inside this function
-%function out = minimize(P, CL, P_shape, CL_shape)
-function out = minimize(cguess_flat, P_shape, CL_shape)
+%function out = minimize(cguess_flat, P_shape, CL_shape) ;
+function out = minimize(cguess_flat)
 
 %filename = 'C:\Users\sarmc412\OneDrive\liver\practice.csv';
 %C = xlsread(filename)  ;
-% above is for use in optimizer when we are done with mini
 
-C = [50 20 10;50 50 50;5 8 9;60 72 76]  ;
-%change this part above when done with mini
+P_shape = [4 2] ;
+prod_ps = 8 ;  %doing this because MATLAB won't accept to do prod of P_shape
+CL_shape = [2 3] ;   %had to do this manually for now because don't know how to add other input to opt.
+C = [50 20 10;50 50 50;5 8 9;60 72 76]  ;    %change this part above when done with mini
 
 %%% reshaping arrays %%%
-P_flat = cguess_flat(1:numel(P))
-CL_flat = cguess_flat(numel(P):end)
-P = reshape(P_flat, P_shape);
-CL = reshape(CL_flat, CL_shape);
+P_flat = cguess_flat(1:prod_ps) ;
+CL_flat = cguess_flat(prod_ps+1:end) ;   %had to add one just for matlab indexing purposes, gives same values from CL
 
+P_new = reshape(P_flat, P_shape) ;
+CL_new = reshape(CL_flat, CL_shape) ;
 
-
-
-%{
 
 %%% actual optimizer %%%
-cguess = P * CL    
-weight = 1 ./ cguess 
-out = sum(sum((abs(cguess - C).^2) .* weight)) ;    % I did abs here because it feels like you just want to know the diff 
+cguess = P_new * CL_new ;
+weight = 1 ./ cguess ;
+out = sum(sum((abs(cguess - C).^2) .* weight)) ;    
+%did abs here because it feels like you just want to know the diff 
 %did sum twice because it was a matrix issue, might fix when flattening
 
 %}
